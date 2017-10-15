a8d8a8275a900ac76259b803c18a240e1b474a14
Jihye Park <bgh0881@naver.com>
2017-10-16 00:26:10 +0900

    링크는 걸었는데 사진은 아직

--- src/Novel.js
+++ src/Novel.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Novel = () => {
     return (
         <div>
-
+          <table className="table">
+            <tr>
+              <td><a href="http://page.kakao.com"><div>카카오페이지<div className="Image"><img src="chrome-search://thumb2/http://page.kakao.com"/></div></div></a></td>
+              <td><a href="http://novel.naver.com/webnovel"><div>네이버 웹소설<img src="chrome-search://thumb2/http://novel.naver.com/webnovel"/></div></a></td>
+              <td><div className="empty"></div></td>
+            </tr>
+            <tr>
+              <td><div className="empty"></div></td>
+              <td><div className="empty"></div></td>
+              <td><div className="empty"></div></td>
+            </tr>
+          </table>
         </div>
     );
 };
--- src/Webtoon.js
+++ src/Webtoon.js
@@ -3,7 +3,18 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-
+          <table className="table">
+            <tr>
+              <td><a href="http://comic.naver.com"><div>네이버 웹툰</div></a></td>
+              <td><div className="empty"></div></td>
+              <td><div className="empty"></div></td>
+            </tr>
+            <tr>
+              <td><div className="empty"></div></td>
+              <td><div className="empty"></div></td>
+              <td><div className="empty"></div></td>
+            </tr>
+          </table>
         </div>
     );
 };
